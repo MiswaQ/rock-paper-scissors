8018a87f9a71a21a304baef3a0ca982ccd198309
MiswaQ <miswaq@gmail.com>
2023-03-15 14:45:31 +0000

    Add const to use in js and changed some class/id name

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
-console.log('Connected!')
+/**
+ * Calling DOM for different options.
+ */
+const gameArea = document.getElementsByClassName('game-area')
+const whoWins = document.getElementsByClassName('who-wins')
+const playArea = document.getElementsByClassName('play-area')
+const player = document.getElementsByClassName('player')
+const computer = document.getElementsByClassName('computer')
+const playerImg = document.getElementById('player-img')
+const compImg = document.getElementById('comp-img')
+const playOptions = document.getElementsByClassName('play-options')
+const choice = document.getElementsByClassName('choice')
+const scoreArea = document.getElementsByClassName('score-area')
